750d18068a498ab31040791d1b377b32e5543fa4
erobinofficial <erobin8778@gmail.com>
2023-01-26 02:09:23 +0600

    more

--- method.js
+++ method.js
@@ -2,18 +2,24 @@ const person = {
   name: "Razu Chakraborty",
   address: "Jessore",
   age: 22,
+  balance: 10000,
   gender: "male",
   hobbies: ["coding", "Pinch Mara", "Travelling"],
   behaviours: function () {
-    return `I'm ${this.name} and I'm ${this.age}`;
+    return `I'm ${this.name} and I'm ${this.age}.`;
   },
   overLap: function (hobbiess) {
-    this.behaviours();
-    return `${this.name} and My hobbies is ${hobbiess}`;
+    return `${this.behaviours()} My hobbies is ${hobbiess}`;
   },
+  book: function (amount) {
+    this.balance = this.balance - amount;
+    return this.balance;    
+  }
 };
 // console.log(person.name);
-// person.behaviours();
+// console.log(person.behaviours());
 const behaviours = person.behaviours();
 const overLap = person.overLap("Pinch Mara");
 console.log(overLap);
+// console.log(person.book(900));
+
