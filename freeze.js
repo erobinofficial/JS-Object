5d40d7309ae204797fb84ffb23789328dfe5b94b
erobinofficial <erobin8778@gmail.com>
2023-01-26 21:29:58 +0600

    object

---
+++ freeze.js
@@ -0,0 +1,15 @@
+const phone = {Name: 'Pixel', Brand: 'Google', Model: 'Pixel 3', Color: 'Not Pink', Variation: 'USA Verizon'};
+const keys = Object.keys(phone);
+// console.log(keys);
+const values = Object.values(phone);
+// console.log(values);
+const pairs = Object.entries(phone);
+// console.log(pairs);
+
+console.log(phone);
+// Object.freeze(phone);
+// Object.seal(phone);
+// delete phone.Color;
+phone.Price = 999;
+phone.Model = 'Pixel 4';
+console.log(phone);
